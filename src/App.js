12be9e6240e2b9ef87bbb84b62cd74da8e65bf29
Rob Williams <92275209+RobWilliams0615@users.noreply.github.com>
2023-01-24 14:35:54 -0700

    added a handle function recieving a prop of title and console log to test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import { useState } from 'react';
 
 function App() {
   const [books, setBooks] = useState([]);
+
+  const handleCreateBook = (title) => {
+    console.log('add book with', title);
+  };
+
   return <div>App</div>;
 }
 
